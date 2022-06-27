a660404937e51b2fe29c3461a49da2f3ea27d2ea
Maulik Bhatt <101645973+MaulikBhatt7@users.noreply.github.com>
2022-06-27 20:50:17 +0530

    Update HappyNumber.java

--- Data Structure/HappyNumber.java
+++ Data Structure/HappyNumber.java
@@ -5,17 +5,16 @@ class HappyNumber{
 		System.out.print("Enter number : ");
 		int n=sc.nextInt();
 		int sum[]=new int[100];
-		sum[0]=0;
 		int temp=0;
 		int r;
 		boolean flag=true;
-			for(int i=1;i<100;i++){
+			for(int i=0;i<100;i++){
 				while(n!=0){
 					r=n%10;
 					n=n/10;
 					temp+=r*r;
 					sum[i]=temp;
-					for(int j=1;j<i;j++){
+					for(int j=0;j<i;j++){
 						if(sum[j]==temp){
 							flag=false;
 							break;

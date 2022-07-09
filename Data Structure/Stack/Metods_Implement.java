e49e7846aeda134ac470336e7c08bd337e48e37e
Maulik Bhatt <101645973+MaulikBhatt7@users.noreply.github.com>
2022-07-09 16:38:37 +0530

    Update Metods_Implement.java

--- Data Structure/Stack/Metods_Implement.java
+++ Data Structure/Stack/Metods_Implement.java
@@ -1,6 +1,6 @@
 class Method_Implement_Demo{
-	static int a[]=new int[100];
-	static int top=-1
+	static int a[]=new int[10];
+	static int top=-1;
 	public static void push(int n){
 		if(top>=10){
 			System.out.println("Stack Overflow");
@@ -12,6 +12,7 @@ class Method_Implement_Demo{
 	public static int pop(){
 		if(top<0){
 			System.out.println("Stack Underflow");
+			return 0;
 		}
 		else{
 			return a[top--];
@@ -20,6 +21,7 @@ class Method_Implement_Demo{
 	public static int peek(){
 		if(top<0){
 			System.out.println("Stack Underflow");
+			return 0;
 		}
 		else{
 			return a[top];
@@ -28,6 +30,7 @@ class Method_Implement_Demo{
 	public static int peep(int i){
 		if(top-i+1<0){
 			System.out.println("Stack Underflow");
+			return 0;
 		}
 		else{
 			return a[top-i+1];
@@ -47,16 +50,24 @@ class Method_Implement_Demo{
 		}
 	}
 }
-class Method_Implement_{
+class Method_Implement{
 	public static void main(String[] args) {
 		Method_Implement_Demo mid=new Method_Implement_Demo();
 		mid.push(5);
+		mid.push(10);
+		mid.push(15);
+		mid.push(10);
+		mid.push(14);
+		mid.push(11);
+		mid.push(12);
+		mid.push(20);
+		mid.push(30);
+		mid.push(40);
 		mid.pop();
 		mid.peek();
 		mid.peep(1);
 		mid.change(1,2);
-		mid.display;
+		mid.display();
 	}
 }
-
 
